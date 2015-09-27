9df8a653c85cbabe23133e3bbe09db757b6e4bd3
tchambill <antony_epis@hotmail.com>
2015-09-27 01:38:20 -0300

    add function backus for data base

---
+++ SisControlPresupuestal/WinUI/frmBackup.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WinUI
+{
+    partial class frmBackup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExec = new System.Windows.Forms.Button();
+            this.txtpath = new System.Windows.Forms.TextBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExec
+            // 
+            this.btnExec.Location = new System.Drawing.Point(159, 116);
+            this.btnExec.Name = "btnExec";
+            this.btnExec.Size = new System.Drawing.Size(152, 23);
+            this.btnExec.TabIndex = 2;
+            this.btnExec.Text = "Execution";
+            this.btnExec.UseVisualStyleBackColor = true;
+            this.btnExec.Click += new System.EventHandler(this.btnExec_Click);
+            // 
+            // txtpath
+            // 
+            this.txtpath.Location = new System.Drawing.Point(48, 81);
+            this.txtpath.Name = "txtpath";
+            this.txtpath.Size = new System.Drawing.Size(361, 20);
+            this.txtpath.TabIndex = 4;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(435, 79);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(75, 23);
+            this.btnadd.TabIndex = 5;
+            this.btnadd.Text = "...";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // frmBackup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 240);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.txtpath);
+            this.Controls.Add(this.btnExec);
+            this.Name = "frmBackup";
+            this.Text = "frmBackup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnExec;
+        private System.Windows.Forms.TextBox txtpath;
+        private System.Windows.Forms.Button btnadd;
+    }
+}
---
+++ SisControlPresupuestal/WinUI/frmBackup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using BUS;
+namespace WinUI
+{
+    public partial class frmBackup : Form
+    {
+        public frmBackup()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void btnExec_Click(object sender, EventArgs e)
+        {
+        
+            if (!string.IsNullOrEmpty(this.txtpath.Text.Trim()))
+            {
+                if (new Backup_BUS().BackupGenerate(this.txtpath.Text.Trim()))
+                {
+                    //activate controls
+                    MessageBox.Show("ok");
+                }
+            }
+        }
+
+        
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            try {
+                FolderBrowserDialog fdialog = new FolderBrowserDialog
+                {
+                    RootFolder = Environment.SpecialFolder.MyComputer,
+                    ShowNewFolderButton = false
+                };
+                if (fdialog.ShowDialog() == DialogResult.OK)
+                {
+                    string spath = fdialog.SelectedPath;
+                    this.txtpath.Text = Path.Combine(spath, "Data_" + DateTime.Today.ToString("ddMMyyyy") + ".bak");
+                }
+                }
+            catch (Exception ex) {
+                throw ex;
+            }
+        }
+    }
+}
